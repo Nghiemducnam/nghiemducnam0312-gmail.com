9be069f59017447bff5acda39817c0d6f92e7009
Nghiemducnam <Nghiemducnam0312@gmail.com>
2020-03-30 10:48:28 +0700

    add output component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OutputComponent } from './output/output.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OutputComponent
   ],
   imports: [
     BrowserModule,
